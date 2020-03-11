4ba954162550160db739f6c6317b92030c7f2173
mor bargig <morbargig@gmail.com>
2020-03-12 00:23:15 +0200

    heroku

--- README.md
+++ README.md
@@ -1,41 +1,41 @@
-## [Mor Bargig SMS Services](https://sms-services.herokuapp.com/)
+REM ## [Mor Bargig SMS Services](https://sms-services.herokuapp.com/)
 
-[![Demo CountPages alpha](https://media.giphy.com/media/MaxtROcFIMsz2DOG4e/giphy.gif)](https://sms-services.herokuapp.com/)
+REM [![Demo CountPages alpha](https://media.giphy.com/media/MaxtROcFIMsz2DOG4e/giphy.gif)](https://sms-services.herokuapp.com/)
 
 
-web site for my needs, so I can send to customers SMS with details and links to parties for my work in the night life.
+REM web site for my needs, so I can send to customers SMS with details and links to parties for my work in the night life.
 
-want to use this website ? open cmd and run `git clone https://github.com/morbargig/SMS-Services.git ` to install the product 
+REM want to use this website ? open cmd and run `git clone https://github.com/morbargig/SMS-Services.git ` to install the product 
 
-after you need to install all the libraries, run `npm install`
+REM after you need to install all the libraries, run `npm install`
 
-run `npm start` to run the app
+REM run `npm start` to run the app
 
-run `node server.js` to run the server
+REM run `node server.js` to run the server
 
-you need to fill this fields with your Nexmo account details 
-```javascript
-const Nexmo = require('nexmo');
-const nexmo = new Nexmo({
-    apiKey: '??',
-    apiSecret: '???',
-});
-```
+REM you need to fill this fields with your Nexmo account details 
+REM ```javascript
+REM const Nexmo = require('nexmo');
+REM const nexmo = new Nexmo({
+REM     apiKey: '??',
+REM     apiSecret: '???',
+REM });
+REM ```
 
-and don't forget to send the remove link 🔗 if you need to :)
+REM and don't forget to send the remove link 🔗 if you need to :)
 
-## Goals :    
-- 09/03/2020
-    - [X] Facebook Login
-    - [X] firebase database 
-    - [X] remove links 🔗 
-    - [ ] sms services 
-    - [ ] Design 
-    - [ ] Credit card payment
+REM ## Goals :    
+REM - 09/03/2020
+REM     - [X] Facebook Login
+REM     - [X] firebase database 
+REM     - [X] remove links 🔗 
+REM     - [ ] sms services 
+REM     - [ ] Design 
+REM     - [ ] Credit card payment
 
-### Remove Page
+REM ### Remove Page
 
-![alt text](./photos/remove.png)
+REM ![alt text](./photos/remove.png)
 
     
     
--- src/component/Admin.jsx
+++ src/component/Admin.jsx
@@ -13,210 +13,210 @@ class Admin extends Component {
         }
     }
 
-    // componentDidMount = async () => {
-    //     firebase.database().ref('users/sms_users/').on('value', (snap) => {
-    //         // console.log(snap.val());
-    //         let { displayName, uid } = this.props.state.user
-    //         // console.log(user)
-    //         if (snap.val() !== null && snap.val()[displayName + "-" + uid] !== null && snap.val()[displayName + "-" + uid] !== undefined) {
-    //             this.setState({
-    //                 broadcastLists: snap.val()[displayName + "-" + uid].broadcastLists
-    //             })
-    //         }
-    //     });
-    // }
+    componentDidMount = async () => {
+        firebase.database().ref('users/sms_users/').on('value', (snap) => {
+            // console.log(snap.val());
+            let { displayName, uid } = this.props.state.user
+            // console.log(user)
+            if (snap.val() !== null && snap.val()[displayName + "-" + uid] !== null && snap.val()[displayName + "-" + uid] !== undefined) {
+                this.setState({
+                    broadcastLists: snap.val()[displayName + "-" + uid].broadcastLists
+                })
+            }
+        });
+    }
 
 
 
 
 
-    // sendMassege = async (e) => {
-    //     let from = prompt("Please enter from who this massage");
-    //     let name = e.target.name
-    //     let to = "972528612379"
-    //     if (name === 'sendMassege') { to = 1 }
-    //     await axios.post(`${route}sendSms/${from}/${to}`, {
-    //         text: this.state.text
-    //     })
-    // }
+    sendMassege = async (e) => {
+        let from = prompt("Please enter from who this massage");
+        let name = e.target.name
+        let to = "972528612379"
+        if (name === 'sendMassege') { to = 1 }
+        await axios.post(`${route}sendSms/${from}/${to}`, {
+            text: this.state.text
+        })
+    }
 
-    // showUsers = () => {
-    //     this.setState(prevState => ({
-    //         showUsers: !prevState.showUsers
-    //     }))
-    // }
+    showUsers = () => {
+        this.setState(prevState => ({
+            showUsers: !prevState.showUsers
+        }))
+    }
 
-    // addUsers = async (toAddOrToUpdate) => {
-    //     let users = this.state.listOfUsers.split("\n")
-    //     users = users.map(i => i.split(","))
-    //     let erorrNmbers = []
-    //     for (let i of users) {
-    //         // let u = i[1]
-    //         i[1] = i[1].replace("+", "").replace(/-/g, '').replace(" ", "")
-    //         // console.log(u)
-    //         if (i[1][0] === '+') { i[1] = i[1].slice(4, 13) }
-    //         i[1][0] + i[1][1] + i[1][2] === '972' ? i[1] = i[1].slice(3, 12) : i[1] = i[1].slice(1, 10)
-    //         // console.log(i[1].length, i[1])
-    //         if (i[1][0] === "5" && i[1].length === 9) {
-    //             i[1] = parseInt(i[1])
-    //             // console.log(i[1])
-    //         } else { i[1] = null; erorrNmbers.push(i) }
-    //     }
-    //     users = users.filter(i => i[1] !== null)
-    //     // console.log(users)
-    //     if (erorrNmbers === []) {
-    //         alert(`that use problem with users ${erorrNmbers}
-    //         \n saved ${users.length} users`)
-    //     }
-    //     let data
-    //     let { uid } = this.props.state.user
-    //     let broadcastList = this.state.broadcastList
-    //     await firebase.database().ref('users/sms_broadcast/').once('value').then(function (snap) {
+    addUsers = async (toAddOrToUpdate) => {
+        let users = this.state.listOfUsers.split("\n")
+        users = users.map(i => i.split(","))
+        let erorrNmbers = []
+        for (let i of users) {
+            // let u = i[1]
+            i[1] = i[1].replace("+", "").replace(/-/g, '').replace(" ", "")
+            // console.log(u)
+            if (i[1][0] === '+') { i[1] = i[1].slice(4, 13) }
+            i[1][0] + i[1][1] + i[1][2] === '972' ? i[1] = i[1].slice(3, 12) : i[1] = i[1].slice(1, 10)
+            // console.log(i[1].length, i[1])
+            if (i[1][0] === "5" && i[1].length === 9) {
+                i[1] = parseInt(i[1])
+                // console.log(i[1])
+            } else { i[1] = null; erorrNmbers.push(i) }
+        }
+        users = users.filter(i => i[1] !== null)
+        // console.log(users)
+        if (erorrNmbers === []) {
+            alert(`that use problem with users ${erorrNmbers}
+            \n saved ${users.length} users`)
+        }
+        let data
+        let { uid } = this.props.state.user
+        let broadcastList = this.state.broadcastList
+        await firebase.database().ref('users/sms_broadcast/').once('value').then(function (snap) {
 
-    //         if (snap.val() !== null) {
-    //             data = snap.val()[broadcastList + "-" + uid]
-    //         }
-    //     });
+            if (snap.val() !== null) {
+                data = snap.val()[broadcastList + "-" + uid]
+            }
+        });
 
 
-    //     // console.log(data)
-    //     if (data === undefined || !toAddOrToUpdate) {
+        // console.log(data)
+        if (data === undefined || !toAddOrToUpdate) {
 
-    //     } else {
-    //         let isAllreadyIn
-    //         for (let u of users) {
-    //             isAllreadyIn = data.users.filter(i => i[1] === u[1])
-    //             if (isAllreadyIn.length > 0) {
-    //                 u[1] = null
-    //             }
-    //         }
-    //         // console.log(users)
-    //         users = users.filter(i => i[1] !== null)
-    //         // console.log(users)
-    //         users = users.concat(data.users)
-    //         // console.log(users)
-    //     }
-    //     firebase.database().ref('users/sms_broadcast/' + broadcastList + "-" + uid).set({
-    //         users: users
-    //     });
-    //     this.setState({ correnetUsers: users })
+        } else {
+            let isAllreadyIn
+            for (let u of users) {
+                isAllreadyIn = data.users.filter(i => i[1] === u[1])
+                if (isAllreadyIn.length > 0) {
+                    u[1] = null
+                }
+            }
+            // console.log(users)
+            users = users.filter(i => i[1] !== null)
+            // console.log(users)
+            users = users.concat(data.users)
+            // console.log(users)
+        }
+        firebase.database().ref('users/sms_broadcast/' + broadcastList + "-" + uid).set({
+            users: users
+        });
+        this.setState({ correnetUsers: users })
 
-    // }
+    }
 
-    // broadcastSearch = async (e) => {
-    //     let broadcastList = e.target.value
-    //     console.log(broadcastList)
-    //     window.document.getElementById("defaultSelect").remove();
-    //     let { uid } = this.props.state.user
-    //     let data
+    broadcastSearch = async (e) => {
+        let broadcastList = e.target.value
+        console.log(broadcastList)
+        window.document.getElementById("defaultSelect").remove();
+        let { uid } = this.props.state.user
+        let data
 
-    //     await firebase.database().ref('users/sms_broadcast/').once('value').then((snap) => {
-    //         data = snap.val()
-    //         if (data !== undefined && data !== null) {
-    //             data = data[broadcastList + "-" + uid]
-    //         } else {
-    //             this.setState({ broadcastList: broadcastList, correnetUsers: undefined }); return
-    //         }
-    //         // console.log(data)
-    //     })
-    //     if (data !== null && data !== undefined) {
-    //         this.setState({
-    //             broadcastList: broadcastList, correnetUsers: data.users
-    //         })
-    //     } else { this.setState({ broadcastList: broadcastList, correnetUsers: undefined }) }
-    // }
+        await firebase.database().ref('users/sms_broadcast/').once('value').then((snap) => {
+            data = snap.val()
+            if (data !== undefined && data !== null) {
+                data = data[broadcastList + "-" + uid]
+            } else {
+                this.setState({ broadcastList: broadcastList, correnetUsers: undefined }); return
+            }
+            // console.log(data)
+        })
+        if (data !== null && data !== undefined) {
+            this.setState({
+                broadcastList: broadcastList, correnetUsers: data.users
+            })
+        } else { this.setState({ broadcastList: broadcastList, correnetUsers: undefined }) }
+    }
 
-    // addNewBroadcast = async () => {
-    //     if (this.state.newBroadcast === "") { alert("invalid broadcast name"); return }
-    //     let { displayName, uid } = this.props.state.user
-    //     let data
-    //     await firebase.database().ref('users/sms_users/').once('value').then((snap) => {
-    //         data = snap.val()[displayName + "-" + uid]
-    //     });
+    addNewBroadcast = async () => {
+        if (this.state.newBroadcast === "") { alert("invalid broadcast name"); return }
+        let { displayName, uid } = this.props.state.user
+        let data
+        await firebase.database().ref('users/sms_users/').once('value').then((snap) => {
+            data = snap.val()[displayName + "-" + uid]
+        });
 
-    //     let broadcasts
-    //     if (data === undefined || data.broadcastLists === undefined || null) {
-    //         data = this.props.state.user
-    //         broadcasts = [this.state.newBroadcast]
-    //     } else {
-    //         let isAllreadyIn = data.broadcastLists.filter(i => i === this.state.newBroadcast)
-    //         // console.log(isAllreadyIn)
-    //         if (isAllreadyIn.length > 0) {
-    //             alert(`this broadcast alredy in the database`)
-    //             return
-    //         }
-    //         broadcasts = data.broadcastLists
-    //         broadcasts.push(this.state.newBroadcast)
-    //     }
-    //     firebase.database().ref('users/sms_users/' + displayName + "-" + uid).set({
-    //         email: data.email,
-    //         broadcastLists: broadcasts,
-    //         name: data.name ? data.name : data.displayName,
-    //         number: data.number ? data.number : (data.phoneNumber ? data.phoneNumber : 0),
-    //         profile_picture: data.profile_picture ? data.profile_picture : data.photoURL,
-    //         sms_number: data.sms_number ? data.sms_number : 0,
-    //         userId: data.userId ? data.userId : data.uid,
-    //     });
-    //     this.setState({ broadcastLists: broadcasts })
-    // }
+        let broadcasts
+        if (data === undefined || data.broadcastLists === undefined || null) {
+            data = this.props.state.user
+            broadcasts = [this.state.newBroadcast]
+        } else {
+            let isAllreadyIn = data.broadcastLists.filter(i => i === this.state.newBroadcast)
+            // console.log(isAllreadyIn)
+            if (isAllreadyIn.length > 0) {
+                alert(`this broadcast alredy in the database`)
+                return
+            }
+            broadcasts = data.broadcastLists
+            broadcasts.push(this.state.newBroadcast)
+        }
+        firebase.database().ref('users/sms_users/' + displayName + "-" + uid).set({
+            email: data.email,
+            broadcastLists: broadcasts,
+            name: data.name ? data.name : data.displayName,
+            number: data.number ? data.number : (data.phoneNumber ? data.phoneNumber : 0),
+            profile_picture: data.profile_picture ? data.profile_picture : data.photoURL,
+            sms_number: data.sms_number ? data.sms_number : 0,
+            userId: data.userId ? data.userId : data.uid,
+        });
+        this.setState({ broadcastLists: broadcasts })
+    }
 
-    // handleChange = (e) => {
-    //     let name = e.target.name
-    //     let value = e.target.value
-    //     this.setState({ [name]: value })
-    // }
+    handleChange = (e) => {
+        let name = e.target.name
+        let value = e.target.value
+        this.setState({ [name]: value })
+    }
 
-    // addNumber = async () => {
-    //     let { uid } = this.props.state.user
-    //     let broadcastList = this.state.broadcastList
+    addNumber = async () => {
+        let { uid } = this.props.state.user
+        let broadcastList = this.state.broadcastList
 
-    //     let data
-    //     let name = this.state.name
-    //     let number = parseInt(this.state.number)
-    //     let users
-    //     if ((number > 500000000 && number < 599999999) || (number > 972500000000 && number < 972599999999)) {
-    //         if (number >= 972500000000) { number = number - 972000000000 }
-    //         await firebase.database().ref('users/sms_broadcast/').once('value').then(function (snap) {
+        let data
+        let name = this.state.name
+        let number = parseInt(this.state.number)
+        let users
+        if ((number > 500000000 && number < 599999999) || (number > 972500000000 && number < 972599999999)) {
+            if (number >= 972500000000) { number = number - 972000000000 }
+            await firebase.database().ref('users/sms_broadcast/').once('value').then(function (snap) {
 
-    //             if (snap.val() !== null) {
-    //                 data = snap.val()[broadcastList + "-" + uid]
-    //             }
-    //         });
+                if (snap.val() !== null) {
+                    data = snap.val()[broadcastList + "-" + uid]
+                }
+            });
 
-    //         // console.log(data)
-    //         if (data === undefined) {
-    //             users = [[name, number]]
-    //         } else {
-    //             let isAllreadyIn = data.users.filter(i => i[1] === number)
-    //             if (isAllreadyIn.length > 0) {
-    //                 alert(`this number alredy in the database with name of ${isAllreadyIn[0]}`)
-    //                 return
-    //             }
-    //             users = data.users
-    //             users.push([name, number])
-    //         }
-    //         firebase.database().ref('users/sms_broadcast/' + broadcastList + "-" + uid).set({
-    //             users: users
-    //         });
-    //         this.setState({ name: undefined, number: undefined, correnetUsers: users })
-    //     } else { alert("invalid phone number please send agin") }
-    // }
+            // console.log(data)
+            if (data === undefined) {
+                users = [[name, number]]
+            } else {
+                let isAllreadyIn = data.users.filter(i => i[1] === number)
+                if (isAllreadyIn.length > 0) {
+                    alert(`this number alredy in the database with name of ${isAllreadyIn[0]}`)
+                    return
+                }
+                users = data.users
+                users.push([name, number])
+            }
+            firebase.database().ref('users/sms_broadcast/' + broadcastList + "-" + uid).set({
+                users: users
+            });
+            this.setState({ name: undefined, number: undefined, correnetUsers: users })
+        } else { alert("invalid phone number please send agin") }
+    }
 
-    // testRequest = () => {
-    //     axios.get(`${route}test`)
-    //         .then((response) => {
-    //             console.log(response.data);
-    //             console.log(response.status);
-    //             console.log(response.statusText);
-    //             console.log(response.headers);
-    //             console.log(response.config);
-    //         });
-    //     return 
-    // }
+    testRequest = () => {
+        axios.get(`${route}test`)
+            .then((response) => {
+                console.log(response.data);
+                console.log(response.status);
+                console.log(response.statusText);
+                console.log(response.headers);
+                console.log(response.config);
+            });
+        return 
+    }
 
     render() {
         return <div>
-            {/* <button onClick={this.testRequest}> click me</button>
+            <button onClick={this.testRequest}> click me</button>
             <div>
                 <br></br>
                 <div>create a new broadcast list <br></br>
@@ -265,7 +265,7 @@ class Admin extends Component {
                 {this.state.broadcastList ?
                     <button name='sendMassege' onClick={this.sendMassege}> send to all users</button>
                     : null}
-            </div> */}
+            </div>
         </div>
     }
 }
