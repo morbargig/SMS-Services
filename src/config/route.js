5310a316a6bcab574dad622b0f909b10f83cc25c
mor bargig <morbargig@gmail.com>
2020-03-12 17:43:14 +0200

    'automaticcommit'

--- src/config/route.js
+++ src/config/route.js
@@ -1,9 +1,9 @@
 
 let port = 3030
 function isonline(port) {
-    var uri = `/isItOnline`
+    var uri = `http://localhost:${port}/isItOnline`
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", uri, false);
+    xhr.open("GET", uri, true);
     xhr.send(null);
     console.log("route")
     // console.log(xhr.status, xhr.response)
