03983d5431067f7721c0e34f39fe522c30f5b816
mor bargig <morbargig@gmail.com>
2020-03-12 18:06:47 +0200

    heroku

--- src/config/route.js
+++ src/config/route.js
@@ -1,13 +1,12 @@
 
 let port = 3030
 function isonline(port) {
-    var uri = `http://localhost:${port}/isItOnline`
+    var uri = `/isItOnline`
     var xhr = new XMLHttpRequest();
     xhr.open("GET", uri, true);
     xhr.send(null);
     console.log("route")
     console.log(xhr.status, xhr.response)
-    // console.log("3030" > 0)
     if (xhr.status == 200 && xhr.response == "server is online") {
         return `http://localhost:${port}/`;
     }
